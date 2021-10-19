227b229aff5c64e8198c2155a23d93fbd4169228
Virgileanda Nur Rifqhi <virgileandanr@gmail.com>
2021-10-20 02:49:01 +0700

    adding paragraph

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 </head>
 <body>
     <h2>Hello World!, It's PHP tho</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque fuga ad eveniet sint consectetur unde autem suscipit nam corrupti voluptates, asperiores magnam earum minima ratione praesentium. Exercitationem assumenda consectetur autem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis recusandae deleniti exercitationem libero. Tempore dolorum placeat dignissimos, quod animi eum quis iusto voluptatem nemo consequatur iure voluptates sit debitis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, consequuntur aliquam eos alias explicabo labore adipisci, quia similique saepe dolores veritatis distinctio quisquam est? Dignissimos non quidem voluptas hic amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat reiciendis explicabo vitae fuga cum quia corrupti nesciunt sit, tempora reprehenderit facere obcaecati, aliquid, rerum atque. Odit consequatur ad ipsa necessitatibus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ea tempore qui quae saepe iure eligendi nam neque commodi facere. Saepe rem similique, quod et deserunt officiis enim ratione vitae!
+    </p>
 </body>
 </html>
